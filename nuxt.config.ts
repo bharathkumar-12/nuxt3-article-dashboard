cc0a8667dcdf8d7aea76a4d88052cb81f6897521
Bharath <bharathkumarakkiraju@gmail.com>
2026-03-21 23:48:45 +0530

    fix: NT-000 something new

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
+  experimental: {
+    appManifest: false,
+  },
+
   // Enable TypeScript strict mode
   typescript: {
     strict: true,
